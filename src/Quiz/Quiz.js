02f7c9d513f17be88edcc030d2e8e316c6812c89
Jacob S <jacob.g.stewart@gmail.com>
2021-05-03 14:28:56 -0400

    rendering data to quiz component

--- src/Quiz/Quiz.js
+++ src/Quiz/Quiz.js
@@ -1,8 +1,13 @@
 import './Quiz.css';
-import React from 'react';
+
+
+import React, {useState} from 'react';
 import shuffle from './shuffleArray.js'
 
 const Quiz = ({quizData}) => {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [showScore, setShowScore] = useState(false);
 
   // format encoded response data
   const formattedData = quizData.map(item => {
@@ -19,9 +24,51 @@ const Quiz = ({quizData}) => {
   });
 
 
+  function handleAnswerSelect(selection, correctAnswer) {
+    if (currentQuestion + 1 < formattedData.length) {
+      if (selection === correctAnswer) {
+        setScore(score + 1);
+        setCurrentQuestion(currentQuestion + 1);
+      } else {
+        setCurrentQuestion(currentQuestion + 1);
+      }
+    } else {
+      setShowScore(true);
+      setCurrentQuestion(0);
+    }
+  }
+
+
   return (
     <div className="Quiz">
-      QUIZ
+      {showScore ? (
+        <div className='score'>
+          <span>
+            You scored {score} out of {formattedData.length}
+          </span>
+          <button>Back to Category Selection</button>
+        </div>
+      ) :
+        (<div className="question-section">
+          <div className="count">
+            <span>Question {currentQuestion + 1} / {formattedData.length}</span>
+          </div>
+          <div className="question">{formattedData[currentQuestion].question}</div>
+          <div className="answers">
+            {
+              formattedData[currentQuestion].allAnswers.map(
+                answer => (
+                  <button
+                    key={answer}
+                    className="answer"
+                    onClick={() => handleAnswerSelect(answer, formattedData[currentQuestion].correctAnswer)}>
+                    {answer}
+                  </button>)
+              )
+            }
+          </div>
+        </div>)
+      }
     </div>
   );
 };
